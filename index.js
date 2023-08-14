e28f3dc687504cf283189c4483f3deff284bb0e9
nazmussaadatcse <nazmus.bd97@gmail.com>
2023-08-15 03:28:22 +0600

    delete cart api fix

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-import {ObjectId} from 'mongodb';
+// import {ObjectId} from 'mongodb';
 const express = require('express');
 const app = express();
 const cors = require('cors');
@@ -10,7 +10,7 @@ app.use(express.json());
 
 // db connect 
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.tjhl6td.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -59,7 +59,7 @@ async function run() {
             res.send(result);
         }
     })
-    
+
     app.delete('/carts/:id',async(req,res)=>{
         const id = req.params.id;
         const query = {_id: new ObjectId(id)};
