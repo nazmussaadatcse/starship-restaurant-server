2b838c1f08edd12393b3630c840e1cd8bdf28505
nazmussaadatcse <nazmus.bd97@gmail.com>
2023-08-27 19:16:07 +0600

    make user to admin

--- index.js
+++ index.js
@@ -32,53 +32,72 @@ async function run() {
     const cartCollection = client.db("starship").collection("carts");
     const usersCollection = client.db("starship").collection("users");
 
-    app.get('/menu', async(req,res)=>{
-        const result = await menuCollection.find().toArray();
-        res.send(result);
+    app.get('/menu', async (req, res) => {
+      const result = await menuCollection.find().toArray();
+      res.send(result);
     })
 
-    app.get('/reviews', async(req,res)=>{
-        const result = await reviewsCollection.find().toArray();
-        res.send(result);
+    app.get('/reviews', async (req, res) => {
+      const result = await reviewsCollection.find().toArray();
+      res.send(result);
     })
 
-    app.post('/users', async(req,res)=>{
+    app.get('/users', async (req, res) => {
+      const result = await usersCollection.find().toArray();
+      res.send(result);
+    })
+    
+    app.patch('/users/admin/:id', async (req, res) => {
+      const id = req.params.id;
+      const filter = {_id: new ObjectId(id)};
+      const updateDoc = {
+        $set:{
+          role: 'admin'
+        },
+      };
+      const result = await usersCollection.updateOne(filter, updateDoc);
+      res.send(result);
+    })
+
+
+    app.post('/users', async (req, res) => {
       const user = req.body;
-      const query = {email: user.email}
-      
+      const query = { email: user.email }
+
       const existingUser = await usersCollection.findOne(query);
-      if(existingUser){
-        return res.send({message: ' user already exist'});
+      if (existingUser) {
+        return res.send({ message: ' user already exist' });
       }
       const result = await usersCollection.insertOne(user);
       res.send(result);
     })
 
+
     // cart collection
-    app.post('/carts',async(req,res)=>{
-        const item = req.body;
-        // console.log(item);
-        const result = await cartCollection.insertOne(item);
-        res.send(result);
+    app.post('/carts', async (req, res) => {
+      const item = req.body;
+      // console.log(item);
+      const result = await cartCollection.insertOne(item);
+      res.send(result);
     })
 
-    app.get('/carts',async(req,res)=>{
-        const email = req.query.email;
-        if(!email){
-            res.send([]);
-        }
-        else{
-            const query = { email:email };
-            const result = await cartCollection.find(query).toArray();
-            res.send(result);
-        }
+    app.get('/carts', async (req, res) => {
+      const email = req.query.email;
+      if (!email) {
+        res.send([]);
+      }
+      else {
+        const query = { email: email };
+        const result = await cartCollection.find(query).toArray();
+        res.send(result);
+      }
     })
 
-    app.delete('/carts/:id',async(req,res)=>{
-        const id = req.params.id;
-        const query = {_id: new ObjectId(id)};
-        const result = await cartCollection.deleteOne(query);
-        res.send(result);
+    app.delete('/carts/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await cartCollection.deleteOne(query);
+      res.send(result);
     })
 
 
@@ -95,11 +114,11 @@ run().catch(console.dir);
 // db connect 
 
 
-app.get('/',(req,res)=>{
-    res.send('server is running')
+app.get('/', (req, res) => {
+  res.send('server is running')
 })
 
-app.listen(port,()=>{
-    console.log(`port ${port} online`)
-    console.log('waiting for ping..');
+app.listen(port, () => {
+  console.log(`port ${port} online`)
+  console.log('waiting for ping..');
 })
