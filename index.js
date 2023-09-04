27d7029c076991fddecb7bc3f20342f44ed28841
nazmussaadatcse <nazmus.bd97@gmail.com>
2023-09-05 01:46:01 +0600

    jwt

--- index.js
+++ index.js
@@ -4,9 +4,27 @@ const app = express();
 const cors = require('cors');
 require('dotenv').config();
 const port = process.env.PORT || 5000;
+const jwt = require('jsonwebtoken');
+
 // middle ware  
 app.use(cors());
 app.use(express.json());
+
+const verifyJWT = (req, res, next) => {
+  const authorization = req.headers.authorization;
+  if (!authorization) {
+    return res.status(401).send({ error: true, message: ' unauthorized access' });
+  }
+  // bearer token 
+  const token = authorization.split(' ')[1];
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+    if (err) {
+      return res.status(401).send({ error: true, message: ' unauthorized access' });
+    }
+    req.decoded = decoded;
+    next();
+  })
+}
 
 // db connect 
 
@@ -46,12 +64,12 @@ async function run() {
       const result = await usersCollection.find().toArray();
       res.send(result);
     })
-    
+
     app.patch('/users/admin/:id', async (req, res) => {
       const id = req.params.id;
-      const filter = {_id: new ObjectId(id)};
+      const filter = { _id: new ObjectId(id) };
       const updateDoc = {
-        $set:{
+        $set: {
           role: 'admin'
         },
       };
@@ -59,12 +77,20 @@ async function run() {
       res.send(result);
     })
 
+    app.post('/jwt', (req, res) => {
+      const user = req.body;
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+        expiresIn: 3600
+      })
+      res.send({ token });
+    })
 
     app.post('/users', async (req, res) => {
       const user = req.body;
       const query = { email: user.email }
 
       const existingUser = await usersCollection.findOne(query);
+      console.log('existingUser: ', existingUser);
       if (existingUser) {
         return res.send({ message: ' user already exist' });
       }
@@ -81,16 +107,20 @@ async function run() {
       res.send(result);
     })
 
-    app.get('/carts', async (req, res) => {
+    app.get('/carts', verifyJWT, async (req, res) => {
       const email = req.query.email;
       if (!email) {
         res.send([]);
       }
-      else {
+
+      const decodedEmail = req.decoded.email;
+      if(email != decodedEmail){
+        return res.status(403).send({ error: true, message: ' forbidden access' });
+      }
+  
         const query = { email: email };
         const result = await cartCollection.find(query).toArray();
         res.send(result);
-      }
     })
 
     app.delete('/carts/:id', async (req, res) => {
