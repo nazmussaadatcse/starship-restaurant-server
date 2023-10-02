91cc1fd518fa53c89b78028bfc80a0cf3a81019d
nazmussaadatcse <nazmus.bd97@gmail.com>
2023-10-02 12:18:48 +0600

    payment api detail save to server

--- index.js
+++ index.js
@@ -199,8 +199,12 @@ async function run() {
     // payment api 
     app.post('/payments', verifyJWT, async (req, res) => {
       const payment = req.body;
-      const result = await paymentCollection.insertOne(payment)
-      res.send(result);
+      const insertResult = await paymentCollection.insertOne(payment)
+      
+      const query = {_id:{$in: payment.cartItems.map(id=> new ObjectId(id))}}
+      const deleteResult = await cartCollection.deleteMany(query)
+
+      res.send({insertResult, deleteResult});
     })
 
     // Send a ping to confirm a successful connection
